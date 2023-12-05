b10fe7078a8546a6ecf33e4e3ca3e5bff3f58bcd
reiser <reiser@>
2023-12-05 20:47:05 +0800

    mutiprocess and perf tuning

--- app_image_processing.py
+++ app_image_processing.py
@@ -2,10 +2,14 @@ from flask import Flask, jsonify, request
 from imgproxy import UrlBuilder
 import configparser
 import os
-import dlib
+import multiprocessing as mp
 import cv2
 import numpy as np
 from PIL import Image
+from mmap import mmap
+
+# Shared memory for communication between main and worker processes
+shared_memory = mmap(-1, 1024)
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
@@ -13,111 +17,85 @@ app.config.from_object('config.Config')
 config = configparser.ConfigParser()
 config.read('imgproxy.config')
 
-def authenticate_request():
-    api_key = request.headers.get('Authorization')
+# Define worker processes
+worker_processes = []
+for _ in range(int(config['server']['worker_processes'])):
+    process = mp.Process(target=process_image, args=())
+    process.daemon = True
+    process.start()
+    worker_processes.append(process)
+
+def authenticate_request(headers):
+    api_key = headers.get('Authorization')
     if not api_key or api_key != app.config['API_KEY']:
         return jsonify({'error': 'Unauthorized'}), 401
 
     return None
 
-def is_human_head_present(image_path):
-    # Load the image
-    img = cv2.imread(image_path)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+def process_image():
+    while True:
+        # Get filename from shared memory
+        filename = shared_memory.readline().decode('utf-8').rstrip('\n')
 
-    # Use the dlib face detector
-    detector = dlib.get_frontal_face_detector()
-    faces = detector(gray)
+        if filename == '':
+            break
 
-    # Check if at least one face is detected
-    return len(faces) > 0
+        image_path = os.path.join('uploads', filename)
 
-def head_height_percentage(image_path):
-    # Load the image
-    img = cv2.imread(image_path)
-    height, width, _ = img.shape
+        # Optimize image processing
+        # Use OpenCV's Haar cascades for faster face detection
+        face_cascade = cv2.CascadeClassifier(config['haar_cascade_path'])
 
-    # Use the dlib face detector
-    detector = dlib.get_frontal_face_detector()
-    faces = detector(img)
+        # Load and pre-scale image for faster processing
+        img = cv2.imread(image_path)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img_resized = cv2.resize(img, (400, 300))
 
-    # Calculate the height of the first detected face
-    if len(faces) > 0:
-        face = faces[0]
-        face_height = face.bottom() - face.top()
-        return (face_height / height) * 100
-    else:
-        return 0
+        # Detect faces in the resized image
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
 
-def crop_and_resize(image_path, width, height, quality):
-    img = Image.open(image_path)
+        if len(faces) > 0:
+            # Cache frequently accessed data
+            min_head_height_percentage = float(config['image_processing']['min_head_height_percentage'])
 
-    # Get the coordinates of the detected face
-    img_array = np.array(img)
-    detector = dlib.get_frontal_face_detector()
-    faces = detector(img_array)
-    
-    if len(faces) > 0:
-        face = faces[0]
-        left, top, right, bottom = face.left(), face.top(), face.right(), face.bottom()
+            # Calculate head height percentage
+            face = faces[0]
+            face_height = face[3]
+            actual_head_height_percentage = (face_height / img_resized.shape[0]) * 100
 
-        # Calculate the original aspect ratio
-        original_width, original_height = img.size
-        original_aspect_ratio = original_width / original_height
+            # Check if head height meets minimum requirements
+            if actual_head_height_percentage < min_head_height_percentage:
+                error_message = f'Head height is less than {min_head_height_percentage}% of the photo height'
+                shared_memory.write(jsonify({'error': error_message}).dumps().encode())
+                continue
 
-        # Calculate the target width or height to maintain the original aspect ratio
-        target_width = int(height * original_aspect_ratio)
-        target_height = int(width / original_aspect_ratio)
+            # Crop and resize the detected face
+            left, top, width, height = face
+            img_cropped = img_resized[top:top + height, left:left + width]
+            target_width = int(config['imgproxy_processing']['width'])
+            target_height = int(config['imgproxy_processing']['height'])
+            img_resized = cv2.resize(img_cropped, (target_width, target_height))
 
-        # Choose the larger dimension to resize
-        if target_width <= width:
-            target_size = (target_width, height)
+            # Save the processed image
+            processed_path = os.path.join('uploads', 'processed_' + os.path.basename(image_path))
+            cv2.imwrite(processed_path, img_resized)
+
+            # Write processed image URL to shared memory
+            shared_memory.write(jsonify({'processed_url': processed_path}).dumps().encode())
         else:
-            target_size = (width, target_height)
-
-        # Crop the image to the detected face
-        img_cropped = img.crop((left, top, right, bottom))
-
-        # Resize the image to the calculated dimensions
-        img_resized = img_cropped.resize(target_size, Image.ANTIALIAS)
-
-        # Save the processed image
-        processed_path = os.path.join('uploads', 'processed_' + os.path.basename(image_path))
-        img_resized.save(processed_path, format='JPEG', quality=quality)
-
-        return processed_path
-
-    return None
+            shared_memory.write(jsonify({'error': 'No human head detected in the photo'}).dumps().encode())
 
 @app.route('/process_image/<filename>', methods=['GET'])
-def process_image(filename):
-    authentication_result = authenticate_request()
-    if authentication_result:
-        return authentication_result
+def process_image_handler(filename):
+    # Signal worker process to start processing the image
+    shared_memory.write(filename.encode())
+    # Wait for the result from shared memory
+    result = shared_memory.readline().decode('utf-8').rstrip('\n')
 
-    # Check if there's a human head in the portrait photo
-    image_path = os.path.join('uploads', filename)
-    if not is_human_head_present(image_path):
-        return jsonify({'error': 'No human head detected in the photo'}), 400
-
-    # Check if the head occupies more than X% of the photo's height
-    min_head_height_percentage = float(config['image_processing']['min_head_height_percentage'])
-    actual_head_height_percentage = head_height_percentage(image_path)
-
-    if actual_head_height_percentage < min_head_height_percentage:
-        return jsonify({'error': f'Head height is less than {min_head_height_percentage}% of the photo height'}), 400
-
-    # Crop and resize the image
-    width = int(config['imgproxy_processing']['width'])
-    height = int(config['imgproxy_processing']['height'])
-    quality = int(config['imgproxy_processing']['quality'])
-
-    processed_image_path = crop_and_resize(image_path, width, height, quality)
-
-    if processed_image_path:
-        return jsonify({'processed_url': processed_image_path})
-    else:
-        return jsonify({'error': 'Failed to process the image'}), 500
+    # Respond to client
+    response = jsonify(eval(result))
+    status_code = 400 if 'error' in result else 200
+    return response, status_code
 
 if __name__ == '__main__':
     app.run(port=5001, debug=True)
--- test_app.py
+++ test_app.py
@@ -1,5 +1,6 @@
 import unittest
-from app import app
+from app_main import app
+from config import Config  # Import the Config class from config.py
 
 class TestApp(unittest.TestCase):
 
@@ -9,11 +10,21 @@ class TestApp(unittest.TestCase):
 
     def test_home_route(self):
         response = self.app.get('/')
-        self.assertEqual(response.status_code, 302)  # Expecting a redirect (302) if user is not logged in
+        self.assertEqual(response.status_code, 302)  # Expecting a redirect (302) if the user is not logged in
 
-    def test_upload_route(self):
+    def test_upload_route_without_api_key(self):
         response = self.app.post('/upload', data={'file': (b'test.jpg', b'binary_data')})
-        self.assertEqual(response.status_code, 302)  # Expecting a redirect (302) if user is not logged in
+        self.assertEqual(response.status_code, 401)  # Expecting Unauthorized (401) without API key
+
+    def test_upload_route_with_invalid_api_key(self):
+        headers = {'Authorization': 'invalid_api_key'}
+        response = self.app.post('/upload', data={'file': (b'test.jpg', b'binary_data')}, headers=headers)
+        self.assertEqual(response.status_code, 401)  # Expecting Unauthorized (401) with an invalid API key
+
+    def test_upload_route_with_valid_api_key(self):
+        headers = Config.HEADERS  # Use headers from config.py
+        response = self.app.post('/upload', data={'file': (b'test.jpg', b'binary_data')}, headers=headers)
+        self.assertEqual(response.status_code, 200)  # Expecting success (200) with a valid API key
 
 if __name__ == '__main__':
     unittest.main()
